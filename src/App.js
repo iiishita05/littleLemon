a5bb5324df0b0842c6b1a9674f981bb1328a1bb1
iiishita05 <ishitasrivastava003@gmail.com>
2025-01-15 18:55:20 +0530

    Update scripts in package.json for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./common/Footer";
 import Header from "./common/Header";
-import MainComponent from "./MainComponent"; // Make sure this import is correct.
+import MainComponent from "./MainComponent"; 
 
 function App() {
   return (
